5ef4a9f651ddaf3a009bf2407a0e1a34ac3f1eaa
Sam Wrigley <sam@samwrigley.co.uk>
2018-02-23 11:13:14 +0000

    Imported all Vue filters and `FormatNumber` module and added to Vue install method as well as export individually

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
+import FormatNumber from './Modules/FormatNumber'
+
 import FormMixin from './Mixins/FormMixin'
+
+import DefaultFilter from './Filters/DefaultFilter'
+import MoneyFilter from './Filters/MoneyFilter'
+import PercentageFilter from './Filters/PercentageFilter'
 
 const Maxfactor = {
     install(Vue) {
         Vue.mixin(FormMixin)
+        Vue.filter('default', DefaultFilter)
+        Vue.filter('money', MoneyFilter)
+        Vue.filter('percentage', PercentageFilter)
     },
 }
 
 export default Maxfactor
 
 export {
+    FormatNumber,
     FormMixin,
+    DefaultFilter,
+    MoneyFilter,
+    PercentageFilter,
 }
