9c171f1b9f4d5236f2daa52485b5edd0e596110d
Sam Wrigley <sam@samwrigley.co.uk>
2018-03-20 16:28:22 +0000

    Created `AjaxStore` class as a base class for a Vuex store that requires an AJAX request to get store data from an API end-point

---
+++ src/Store/AjaxStore.js
@@ -0,0 +1,64 @@
+/* eslint-disable no-param-reassign */
+
+import axios from 'axios'
+
+class AjaxStore {
+    constructor(options = {}) {
+        this.action = options.action || ''
+        this.method = options.method || 'GET'
+
+        return this.getStore()
+    }
+
+    getStore() {
+        return {
+            namespaced: true,
+
+            state: {
+                items: [],
+                loading: false,
+                errors: [],
+            },
+
+            getters: {
+                items: state => state.items,
+                hasItems: state => state.items.length !== 0,
+                loading: state => state.loading,
+                errors: state => state.errors,
+            },
+
+            mutations: {
+                updateItems: (state, items) => {
+                    state.items = items
+                },
+                updateLoading: (state, loading) => {
+                    state.loading = loading
+                },
+                updateErrors: (state, errors) => {
+                    state.errors = errors
+                },
+            },
+
+            actions: {
+                updateItems: ({ commit }) => {
+                    commit('updateLoading', true)
+
+                    axios({
+                        method: this.method,
+                        url: this.action,
+                    })
+                        .then((response) => {
+                            commit('updateLoading', false)
+                            commit('updateItems', response.data)
+                        })
+                        .catch((error) => {
+                            commit('updateLoading', false)
+                            commit('updateErrors', error)
+                        })
+                },
+            },
+        }
+    }
+}
+
+export default AjaxStore
