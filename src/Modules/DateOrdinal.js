f5cd625443293883f503b6acbc2e42501eefc031
tristanward <tristan.ward@dewsign.co.uk>
2018-10-30 15:57:08 +0000

    update ordinal module name

--- src/Modules/DateOrdinal.js
+++ src/Modules/DateOrdinal.js
@@ -4,6 +4,6 @@
  * @param  {Integer} n  Get ordinal for this value
  * @return {String}     Input n with ordinal suffix
  */
-const GetOrdinal = n => n + (n > 0 ? ['th', 'st', 'nd', 'rd'][(n > 3 && n < 21) || n % 10 > 3 ? 0 : n % 10] : '')
+const DateOrdinal = n => n + (n > 0 ? ['th', 'st', 'nd', 'rd'][(n > 3 && n < 21) || n % 10 > 3 ? 0 : n % 10] : '')
 
-export default GetOrdinal
+export default DateOrdinal
